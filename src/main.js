0f472083b626a6b809f75284959d107e348d1f4e
yangshaobooo <1783494374@qq.com>
2024-05-14 15:50:04 +0800

    样式文件引入

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+
+//引入初始化样式文件
+import '@/styles/common.scss'
 
 const app = createApp(App)
 
