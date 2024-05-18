de2314be95df6096df7f0ccc46316bf36f4006eb
yangshaobooo <1783494374@qq.com>
2024-05-18 10:38:12 +0800

    精简目录

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,9 @@
 // createRouter 创建router实例对象
 // createWebHistory 创建history模式的路由
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
-import Category from '@/views/Category/index.vue'
-import SubCategory from '@/views/SubCategory/index.vue'
-import Detail from '@/views/Detail/index.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,24 +17,9 @@ const router = createRouter({
           path:'',
           component:Home
         },
-        {
-          path:'category/:id',
-          component:Category
-        },
-        {
-          path:'category/sub/:id',
-          component:SubCategory
-        },
-        {
-          path:'detail/:id',
-          component:Detail
-        }
+
       ]
     },
-    {
-      path: '/login',
-      component: Login
-    }
   ],
   // 路由滚动行为配置项
   scrollBehavior(){
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,13 +1,8 @@
 <script setup>
 import HomeSearch from './components/HomeSearch.vue';
-import HomeNew from './components/HomeNew.vue'
-import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+
 </script>
 
 <template>
   <HomeSearch />
-  <HomeNew />
-  <HomeHot />
-  <homeProduct />
 </template>
