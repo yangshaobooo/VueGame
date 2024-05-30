1c9ec13734671d2f173ccaeb25fc037cde61f2ee
yangshaobooo <1783494374@qq.com>
2024-05-30 14:53:55 +0800

    详情页面-视频和图片

---
+++ src/components/ImageView/index.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { ref } from 'vue'
+// 图片列表
+
+// props适配图片列表 
+defineProps({
+  imageList: {
+    type: Array,
+    default: () => []
+  }
+})
+
+
+// 1、小图切换大图显示
+const activeIndex = ref(0)
+const enterHandler = (i) => {
+  activeIndex.value = i
+}
+</script>
+
+
+<template>
+  <div class="goods-image">
+    <!-- 左侧大图-->
+    <div class="middle" ref="target">
+      <img :src="imageList[activeIndex]" alt="" />
+    </div>
+    <!-- 小图列表 -->
+    <ul class="small">
+      <li v-for="(img, i) in imageList" :key="i" @mouseenter="enterHandler(i)" :class="{ active: i === activeIndex }">
+        <img :src="img" alt="" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.goods-image {
+  margin-top: 25px;
+  width: 900px;
+  position: relative;
+  display: flex;
+  .middle {
+    background: #f5f5f5;
+    img {
+      width: 750px;
+      height: auto;
+    }
+  }
+
+  .small {
+    width: 100px;
+
+    li {
+      width: 100px;
+      height: 57px;
+      margin-left: 18px;
+      margin-bottom: 13px;
+      cursor: pointer;
+
+      &:hover,
+      &.active {
+        border: 2px solid $xtxColor;
+      }
+    }
+  }
+}
+</style>
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref } from 'vue'
 import { getGameDetailAPI } from '@/apis/game'
 import { useRoute } from 'vue-router'
+import ImageView from '@/components/ImageView/index.vue'
 
 const route = useRoute()
 const detail = ref({})
@@ -15,12 +16,72 @@ onMounted(() => getDetail())
 
 <template>
     <h1 v-if="detail.game">{{ detail.game.GameName }}</h1>
+    <div class="box" v-if="detail.game">
+        <div class="video-part">
+            <video :src="detail?.detailParse?.video" controls></video>
+            <div class="game-part">
+                <img :src="detail.game.ImageUrl" alt="" />
+                <p class="introduce">{{ detail.game.Introduce }}</p>
+                <div class="tags">
+                    <el-tag type="warning" effect="dark" round>{{ detail.game.CategoryName }}</el-tag>
+                    <el-tag type="primary" effect="dark" round>游戏大小: {{ detail.game.Size }}</el-tag>
+                    <el-tag type="danger" round>
+                        {{ detail.game.Price === 0 ? '免费游戏' : 'VIP免费' }}
+                    </el-tag>
+                </div>
+            </div>
+        </div>
+        <ImageView :image-list="detail?.detailParse?.pictures" />
+    </div>
+
 </template>
+
 
 <style scoped lang='scss'>
 h1 {
-    background-color: #fff;
-    font-style: italic; 
+    background-color: rgba(27, 40, 56, 1.0);
+    font-style: italic;
     text-align: center;
+    color: #fff;
+    padding-top: 20px;
 }
+
+// 整体布局
+.box {
+    background-color: rgba(27, 40, 56, 1.0);
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+}
+
+// 第一行视频相关布局
+.video-part {
+    padding-top: 20px;
+    width: 900px;
+    display: flex;
+    justify-content: center;
+
+    // 左侧视频
+    video {
+        width: 560px;
+        height: auto;
+    }
+
+    // 右侧游戏描述
+    .game-part {
+        padding-left: 20px;
+        .introduce {
+            margin-top: 10px;
+            color: #fff;
+        }
+
+        .tags {
+            margin: 10px;
+            display: flex;
+            justify-content: space-around;
+        }
+    }
+}
+
+
 </style>
